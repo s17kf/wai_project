c7db7da525043d9a526dbbe9a09cb83ee48bf50b
s17kf <s17kf@users.noreply.github.com>
2023-12-30 14:44:10 +0100

    Remove games' list from nav menu

--- src/views/partial_views/nav_view.php
+++ src/views/partial_views/nav_view.php
@@ -1,15 +1,7 @@
 <nav id="navMenu">
   <ul>
     <li><a href="main" class="active">Strona główna</a></li>
-    <li>
-      <a href="games">Gry</a>
-      <ul class="foldable">
-        <li><a href="games#catan">Catan</a></li>
-        <li><a href="games#cytadela">Cytadela</a></li>
-        <li><a href="games#mars-terraformation">Terraformacja Marsa</a></li>
-        <li><a href="games#unicorns">Odjechane jednorożce</a></li>
-      </ul>
-    </li>
+    <li><a href="games">Gry</a></li>
     <li><a href="survey">Ankieta</a></li>
     <li><a href="gallery">Galeria</a></li>
   </ul>
