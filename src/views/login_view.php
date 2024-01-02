0532d0ceb6f416f9d2391580fd25b8f626064ac3
s17kf <s17kf@users.noreply.github.com>
2024-01-02 23:19:46 +0100

    Remove new user form fata generator file
    Move this function directly to view's file.

--- src/views/login_view.php
+++ src/views/login_view.php
@@ -3,8 +3,11 @@
 
 <?php include_once '../constants.php' ?>
 <?php require '../routing.php' ?>
-<?php require_once 'data/new_user_form_data_generator.php' ?>
+<?php require '../utils/FormEntry.php' ?>
+<?php require '../utils/FormEntryWarned.php' ?>
 
+<?php use utils\FormEntry; ?>
+<?php use utils\FormEntryWarned; ?>
 <head>
   <meta charset="utf-8">
   <title>Gry planszowe i karciane</title>
@@ -21,6 +24,26 @@
     <h1>Gry planszowe i karciane</h1>
   </header>
   <div id="content">
+    <?php
+    $newUserFormEntriesData = [
+      new FormEntryWarned($newUserValidationWarnings['email'] ?? null,
+        "Adres e-mail:", "e-mail", "e-mail", "text", ['value' => $email ?? ""]),
+      new FormEntryWarned($newUserValidationWarnings['login'] ?? null,
+        "Login:", "new_login", "login", "text", ['value' => $new_login ?? ""]),
+      new FormEntryWarned($newUserValidationWarnings['password'] ?? null,
+        "Hasło:", "new_password", "password", "password"),
+      new FormEntryWarned($newUserValidationWarnings['password-repeated'] ?? null,
+        "Powtórz hasło:", "password-repeated", "password-repeated", "password"),
+      new FormEntry("", "submit-button", "submit-button", "submit", ['value' => 'Utwórz konto']),
+    ];
+
+    $loginEntriesData = [
+      new FormEntry("Login:", "login", "login", "text", ['value' => $login ?? ""]),
+      new FormEntry("Hasło:", "password", "password", "password"),
+      new FormEntry("", "submit-button", "submit-button", "submit", ['value' => 'Zaloguj']),
+    ];
+
+    ?>
     <div id="login_form_container">
       <?php if (isset($logged_out)): ?>
         <p class="form-ok-notification">Wylogowano!</p>
